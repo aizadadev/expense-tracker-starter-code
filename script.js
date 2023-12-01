45150f788a688d61a482cd6b7b2d0730b115e5e1
Aizada Iliazova <aiza.iliazova@gmail.com>
2023-11-30 18:11:50 -0800

    generate new transaction

--- script.js
+++ script.js
@@ -6,12 +6,40 @@ const addTransactionForm = document.getElementById('form');
 const newTransactionText = document.getElementById('text');
 const newTransactionAmount = document.getElementById('amount');
 
+let transactions = []
+
+// [
+//   {
+//     id: 1,
+//     text: "Grocery",
+//     amount: -200
+//   },
+//   {
+//     id: 2,
+//     text: "Cash",
+//     amount: 900
+//   }
+// ]
+
 // Add transaction
 function addTransaction(e) {
   e.preventDefault();
-  // Add your code
+  
+  if(newTransactionText.value.trim() === '' || newTransactionAmount.value.trim() === "") {
+    alert("Please add a text and amount")
+  } else {
+    const transaction = {
+      id: generateRandomId(),
+      text: newTransactionText.value,
+      amount: Number(newTransactionAmount.value)
+    };
+    console.log(transaction)
+  }
+}
+
+function generateRandomId() {
+  return Math.floor(Math.random() * 1000)
 }
 
 // Add your code
-
 addTransactionForm.addEventListener('submit', addTransaction);
